e649dd9a129832a733f0fa5e944bb451cd7adb2d
heyyykyyy <90825620+heyyykyyy@users.noreply.github.com>
2021-12-04 16:03:33 +0700

    PERTEMUAN 12

--- ModelBuku.php
+++ ModelBuku.php
@@ -1,5 +1,5 @@
 <?php
-define('BASEPATH') or exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 class ModelBuku extends CI_Model
 {
@@ -8,8 +8,8 @@ class ModelBuku extends CI_Model
     {
         return $this->db->get('buku');
     }
-    
-    public function bukuWhere($where)
+
+    public function bukuWhere()
     {
         return $this->db->get_where('buku', $where);
     }
@@ -23,7 +23,7 @@ class ModelBuku extends CI_Model
     {
         $this->db->update('buku', $data, $where);
     }
-
+    
     public function hapusBuku($where = null)
     {
         $this->db->delete('buku', $where);
@@ -32,10 +32,9 @@ class ModelBuku extends CI_Model
     public function total($field, $where)
     {
         $this->db->select_sum($field);
-        if(!empty($where) && count($where) > 0){
-            $this->db->where($where);
+        if (!empty($where) && count($where) > 0) {
+            $this->db->where->($where);
         }
-
         $this->db->from('buku');
         return $this->db->get()->row($field);
     }
@@ -43,7 +42,7 @@ class ModelBuku extends CI_Model
     //manajemen kategori
     public function getKategori()
     {
-        return $this->db->get->('kategori');
+        return $this->db->get('kategori');
     }
 
     public function kategoriWhere($where)
@@ -53,7 +52,7 @@ class ModelBuku extends CI_Model
 
     public function simpanKategori($data = null)
     {
-        $this->db->delete('kategori', $data);
+        $this->db->insert('kategori', $data);
     }
 
     public function hapusKategori($where = null)
@@ -61,17 +60,17 @@ class ModelBuku extends CI_Model
         $this->db->delete('kategori', $where);
     }
 
-    public function updateKategori($where = null, $data = null)
+    public function updateKategori($wher = null, $data = null)
     {
         $this->db->update('kategori', $data, $where);
     }
+
     //join
     public function joinKategoriBuku($where)
     {
-        $this->db->select('buku.id_kategori,kategori.kategori');
+        $this->db->select('buku.id_kategori, kategori.kategori');
         $this->db->from('buku');
-        $this->db->join('kategori','kategori.id = 
-        buku.id_kategori');
+        $this->db->join('kategori', 'kategori.id = buku.id_kategori');
         $this->db->where($where);
         return $this->db->get();
     }
